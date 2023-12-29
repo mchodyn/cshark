46a3003ed2e849d11ae446a19a7808d6e21dfd19
mchodyn <mchodyn@gmail.com>
2023-12-29 14:35:57 +0100

    Adds initial test cases

---
+++ src/generatePrimes.test.ts
@@ -0,0 +1,14 @@
+import {generatePrimes} from "./generatePrimes"
+
+describe("generatePrimes", function () {
+
+    it("should generate primes from 0 to 10", async () => {
+        const res = await generatePrimes(0, 10)
+        expect(res).toEqual([2,3,5,7])
+    });
+
+    it("shouldn't generate any primes",async () => {
+        const res = await generatePrimes(20,22)
+        expect(res).toEqual([])
+    })
+});
