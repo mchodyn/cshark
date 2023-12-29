aa6aee2209aa065166c2c1ccb10516bb5db8326e
mchodyn <mchodyn@gmail.com>
2023-12-29 14:40:39 +0100

    Lints code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# cshark
+# Notes
+
+App working on node 20.10.0
+
+Things that might be done better 
+- more tests
+- add more comments to explain Sieve of Eratosthenes algorithm used
+- probably I should use Atomics library in order to have safer operations with shared array
+- 
--- src/generatePrimes.test.ts
+++ src/generatePrimes.test.ts
@@ -4,11 +4,11 @@ describe("generatePrimes", function () {
 
     it("should generate primes from 0 to 10", async () => {
         const res = await generatePrimes(0, 10)
-        expect(res).toEqual([2,3,5,7])
+        expect(res).toEqual([2, 3, 5, 7])
     });
 
-    it("shouldn't generate any primes",async () => {
-        const res = await generatePrimes(20,22)
+    it("shouldn't generate any primes", async () => {
+        const res = await generatePrimes(20, 22)
         expect(res).toEqual([])
     })
 });
