807455ddf079e46b6f3b69598b1517ce2fe4d3d8
mchodyn <mchodyn@gmail.com>
2023-12-29 14:36:29 +0100

    Adds prime generation function

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-console.log('Hello world!sdf');
+import os from "os";
+import path from "path";
+import { generatePrimes } from "./generatePrimes";
+
+
+const min = 13;
+const max = 33;
+const primes: number[] = [];
+
+
+
+const run = async () => {
+    const primes = await generatePrimes(min, max);
+    const message = "Prime is : " + primes.join(" ");
+    console.log(message);
+};
+run();
+
+
+
 
